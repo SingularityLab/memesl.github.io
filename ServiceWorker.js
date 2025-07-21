b2b52e7dc660862e37bb8f385b02d3ac7382b347
Elkanoir <kolchinvalery@gmail.com>
2025-07-21 14:53:17 +0300

    Added Ballerina Ultimate

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Quantum-Sports-Arena-Brawler-0.1.0";
 const contentToCache = [
-    "Build/70f7a73039a0a144f9599622b7d57da9.loader.js",
+    "Build/3f54f10cc6d4b058e8d233fbc038912e.loader.js",
     "Build/fa57abfdab4242eea7c9324e17a3c2dd.framework.js",
-    "Build/65ef5185d1b11e3f2e0bbb83e743047e.data",
-    "Build/cef7d9aba978c40825da8a2dfb7174c6.wasm",
+    "Build/954efb1f64148f70bb427dd4ec36bd1f.data",
+    "Build/440bd5402fc7a4b2521cfc2acb3d4554.wasm",
     "TemplateData/style.css"
 
 ];
