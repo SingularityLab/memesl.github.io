3ac431894e2e1a4444745a59df1693d492bf4958
Elkanoir <kolchinvalery@gmail.com>
2025-07-25 12:59:25 +0300

    Try to fix loading error

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Quantum-Sports-Arena-Brawler-0.1.0";
 const contentToCache = [
-    "Build/da69fdada7df6d7ae3200bb050479f96.loader.js",
+    "Build/1f424966ee8c4fd9b52ebbd05d13c005.loader.js",
     "Build/add09181dcaec87c8330b2ea2d613b1f.framework.js.br",
-    "Build/67dc887e1b7bfadaaaacf27302eb34f4.data.br",
-    "Build/358c8d50eb20c94306f3f87d60fc4c65.wasm.br",
+    "Build/8a0de4c8a464df5dcd9aecb9fbcc7f49.data.br",
+    "Build/c4f4db5e914184cd228324f93f61a033.wasm.br",
     "TemplateData/style.css"
 
 ];
