dfe18f86a9ceb6341721f077bbbdb1bb7047ef81
Elkanoir <kolchinvalery@gmail.com>
2025-07-30 15:26:18 +0300

    Fixed missing shaders 3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-Quantum-Sports-Arena-Brawler-0.1.0";
 const contentToCache = [
     "Build/b5c5bbb0f86b602bff0bc7f9b62e337c.loader.js",
     "Build/8ba657418effdb77622a5a3a25c4441c.framework.js",
-    "Build/ebca0a75c536280aecec62618fde9bf5.data",
+    "Build/3817bf4ef9a74d0fab1da9b069b65b4d.data",
     "Build/98988f52ec11b39bf637e1fbe2489782.wasm",
     "TemplateData/style.css"
 
