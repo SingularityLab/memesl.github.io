cb970a29151bdfa01761b98c66c7f81559db7661
Elkanoir <kolchinvalery@gmail.com>
2025-08-04 13:12:09 +0300

    Fixed loading from cache 3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-Quantum-Sports-Arena-Brawler-0.1.0";
 const contentToCache = [
     "Build/cd0d23c5654aedb71440ad8ad5f16005.loader.js",
     "Build/56374c057e759b41f6d4a7f8349cfbff.framework.js",
-    "Build/6469edb52c79d2a32e1bbf1b5c5c37b5.data",
+    "Build/973111b3f8ce5b7aacb1e2e65beb1143.data",
     "Build/49e76b6f862bd9d639f8fc5295f409dc.wasm",
     "TemplateData/style.css"
 
