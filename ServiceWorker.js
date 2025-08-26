593fbb33a6faa3b89e7aa7201ad097255eabb207
Elkanoir <kolchinvalery@gmail.com>
2025-08-26 09:26:44 +0300

    New touch control UI build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Quantum-Sports-Arena-Brawler-0.1.0";
 const contentToCache = [
-    "Build/73ef812e531a43be957099bfa71121f0.loader.js",
+    "Build/3588d1219ad42955007325466037dc7f.loader.js",
     "Build/56374c057e759b41f6d4a7f8349cfbff.framework.js",
-    "Build/a41e5bfa78d0c8bb3298e0ab37babd42.data",
-    "Build/f8e3ac8195734ce85922bdbd8e04a7b2.wasm",
+    "Build/23a28f634d543bc60e9c9868d990090f.data",
+    "Build/fa76ba73829e17b42c5aaffff0c73ed5.wasm",
     "TemplateData/style.css"
 
 ];
